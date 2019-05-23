e05378d24e9ba37e3c513676d017301e26bc0e8f
jsonik <ivan.lopatkin@gmail.com>
2019-05-23 12:12:33 +0500

    add repository link to package.json

--- bin/ecc.js
+++ bin/ecc.js
@@ -4,7 +4,7 @@ const program = require('commander');
 const lib = require('../lib/index.js');
 
 program
-    .version('0.0.1')
+    .version('0.0.11')
     .description('Route and controller creation system. Generate specific API version (route and controller)')
     .option('-f, --file <name>', 'Model file name. Only needed if model name and file name is different, and file name include symbols like _ or -')
     .option('-a, --api <version>', 'Api version (default v1)');
